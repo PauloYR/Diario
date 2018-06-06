ea4c9d4883f2ce59b8f056384fd1dfe50cc1bf78
root <root@localhost.localdomain>
2018-06-06 10:58:08 -0300

    comentarios

--- comentarios/comentarios.php
+++ comentarios/comentarios.php
@@ -1,7 +1,7 @@
 <?php
 require_once('Conexao.php');
 $conexao = new Conexao;
-$sql = "SELECT * FROM comentario";
+$sql = "SELECT * FROM comentario ORDER BY id DESC";
 $comentarios = $conexao->getCon()->query($sql, PDO::FETCH_OBJ);
 foreach($comentarios as $infor){
     echo "<p>$infor->comentario</p>";
